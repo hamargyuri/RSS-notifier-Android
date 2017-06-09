340a5ba12335b5a121d4f3b667ee21244adde384
György Hamar <gyurinak@gmail.com>
2017-06-09 11:22:40 +0200

    initial working service - fixed refresh time, one feed

--- MyApplication/app/src/main/java/hamargyuri/rss_notifier/service/NewFeedNotifierService.java
+++ MyApplication/app/src/main/java/hamargyuri/rss_notifier/service/NewFeedNotifierService.java
@@ -1,25 +1,132 @@
 package hamargyuri.rss_notifier.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.ParseException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import hamargyuri.rss_notifier.R;
+import hamargyuri.rss_notifier.RSSNotifierApp;
+import hamargyuri.rss_notifier.model.DaoSession;
+import hamargyuri.rss_notifier.model.Feed;
+import hamargyuri.rss_notifier.model.FeedDao;
+import hamargyuri.rss_notifier.model.FeedItem;
+import hamargyuri.rss_notifier.model.RSSChannel;
 import hamargyuri.rss_notifier.model.RSSFeed;
 import hamargyuri.rss_notifier.network.RSSFactory;
+import hamargyuri.rss_notifier.view.FeedListActivity;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static hamargyuri.rss_notifier.RSSNotifierApp.TEMP_RSS_TITLE;
+
 public class NewFeedNotifierService extends Service {
+    private DaoSession session = RSSNotifierApp.getSession();
+
     public NewFeedNotifierService() {
     }
 
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        return null;
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+//        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                fetchAndRefreshFeed();
+            }
+        };
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask, 0, 300000); //TODO: 5 minutes - make it changeable
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+
+    //TODO: dynamic, updatable, etc.
+    public void sendNotification(String jobDate) {
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder)
+                new NotificationCompat.Builder(this)
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setSmallIcon(R.drawable.uw_rss_icon)
+                        .setContentTitle("New job on Upwork!")
+                        .setContentText(jobDate);
+
+        int notificationId = 1;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+    private void refreshLatestFeedItem(FeedItem feedItem) {
+        Date latestItemDate = feedItem.getParsedDate();
+
+        FeedDao feedDao = session.getFeedDao();
+        Feed feed = feedDao.queryBuilder().where(FeedDao.Properties.Title.eq(TEMP_RSS_TITLE)).unique();
+        Date previousDate = feed.getLatestItemDate();
+
+        if (previousDate == null || latestItemDate.after(previousDate)) {
+            feed.setLatestItemDate(latestItemDate);
+            feedDao.save(feed);
+            sendNotification(feed.getLatestItemDate().toString());
+        }
+
+        session.clear();
+    }
+
+    private void fetchAndRefreshFeed() {
+        FeedDao feedDao = session.getFeedDao();
+        Feed feed = feedDao.queryBuilder().where(FeedDao.Properties.Title.eq(TEMP_RSS_TITLE)).unique();
+        if (feed == null) {
+            return;
+        }
+
+        String url = feed.getUrl();
+        if (!url.startsWith("http")) url = "https://" + url;
+        session.clear();
+
+        Call<RSSFeed> call = RSSFactory.create().getFeed(url);
+        Callback<RSSFeed> callback = new Callback<RSSFeed>() {
+            @Override
+            public void onResponse(Call<RSSFeed> call, Response<RSSFeed> response) {
+                RSSFeed rssFeed = response.body();
+                if (rssFeed == null) {
+                    Log.d("fetching body", "body is null");
+                    return;
+                }
+                RSSChannel channel = rssFeed.getChannel();
+                if (channel == null) {
+                    Log.d("fetching channel", "channel is null");
+                    return;
+                }
+                FeedItem latestItem = channel.getItems().get(0);
+                refreshLatestFeedItem(latestItem);
+            }
+
+            @Override
+            public void onFailure(Call<RSSFeed> call, Throwable t) {
+                Log.d("callback failure", t.getMessage());
+            }
+        };
+        call.enqueue(callback);
     }
 }
