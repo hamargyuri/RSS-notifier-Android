99396a074102a26e9070cff8d95a5e016b10787c
György Hamar <gyurinak@gmail.com>
2017-06-09 11:21:39 +0200

    added custom application class to run service and handle db

---
+++ MyApplication/app/src/main/java/hamargyuri/rss_notifier/RSSNotifierApp.java
@@ -0,0 +1,35 @@
+package hamargyuri.rss_notifier;
+
+import android.app.Application;
+import android.content.Intent;
+
+import org.greenrobot.greendao.database.Database;
+
+import hamargyuri.rss_notifier.model.DaoMaster;
+import hamargyuri.rss_notifier.model.DaoSession;
+import hamargyuri.rss_notifier.service.NewFeedNotifierService;
+
+/**
+ * Created by hamargyuri on 2017. 06. 07..
+ */
+
+public class RSSNotifierApp extends Application {
+    public static final String TEMP_RSS_TITLE = "upwork"; //TODO: remove
+    private static final String DATABASE = "RSS feeds";
+    private static DaoSession session;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, DATABASE);
+        Database db = helper.getWritableDb();
+        session = new DaoMaster(db).newSession();
+
+        startService(new Intent(this, NewFeedNotifierService.class));
+    }
+
+    public static DaoSession getSession() {
+        return session;
+    }
+}
